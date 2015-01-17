311c7d93b3178ac03697890e4ea669e018688b63
yahiaelgamal <yahiaelgamal@gmail.com>
2015-01-17 19:56:09 +0200

    better code management

--- app.rb
+++ app.rb
@@ -1,3 +1,10 @@
+require 'sinatra'
+require 'haml'
+require 'omniauth'
+require 'koala'
+require 'sinatra/partial'
+require 'awesome_print'
+
 set :haml, layout: true
 
 use Rack::Session::Cookie, secret: 'elsa7elda7emboo'
@@ -7,27 +14,6 @@ APP_SECRET = '3e2b50f67f7d8ee2d38ef3f1944855ea'
 
 get '/' do
   haml :home_page
-end
-
-get '/login' do
-  # generate a new oauth object with your app data and your callback url
-  session['oauth'] = 
-    Koala::Facebook::OAuth.new(APP_ID, APP_SECRET, "#{request.base_url}/callback")
-  # redirect to facebook to get your code
-  redirect session['oauth'].url_for_oauth_code()
-end
-
-get '/logout' do
-  session['oauth'] = nil
-  session['access_token'] = nil
-  redirect '/'
-end
-
-#method to handle the redirect from facebook back to you
-get '/callback' do
-  #get the access token from facebook with your code
-  session['access_token'] = session['oauth'].get_access_token(params[:code])
-  redirect '/'
 end
 
 get '/about' do
@@ -55,3 +41,27 @@ not_found do
   status 404
   haml "404, seriously -.-'"
 end
+
+################################ AUTHENTICATION ###############################
+get '/login' do
+  # generate a new oauth object with your app data and your callback url
+  session['oauth'] = Koala::Facebook::OAuth.new(APP_ID, APP_SECRET,
+                                                "#{request.base_url}/callback")
+  # redirect to facebook to get your code
+  redirect session['oauth'].url_for_oauth_code()
+end
+
+get '/logout' do
+  session['oauth'] = nil
+  session['access_token'] = nil
+  redirect '/'
+end
+
+# method to handle the redirect from facebook back to you
+get '/callback' do
+  #get the access token from facebook with your code
+  session['access_token'] = session['oauth'].get_access_token(params[:code])
+  ap params
+  redirect '/'
+end
+################################ AUTHENTICATION END ###########################
